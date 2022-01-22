cad860c302da3a78e8f442cc75dd61b00518283d
Sahidul Islam <jonyahmed19@gmail.com>
2022-01-22 10:32:51 +0600

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import "./app.css";
+import React from "react";
 import { Routes, Route, Router } from "react-router-dom";
+import "./app.css";
 import Home from "./containers/Home.pages";
 import Contact from "./containers/Contact.pages";
 import Works from "./containers/Works.pages";
